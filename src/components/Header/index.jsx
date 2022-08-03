81ca3bdc3375c63b44b4ed024ea33f0cc653239c
Ahmad Habib <ahmadalkali69@gmail.com>
2022-08-03 09:16:13 +0100

    Created header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,20 @@
+import { Button } from '@mui/material';
 import Navbar from '../Navbar';
+import underline from '../../assets/header/underline.svg';
 
 const Header = () => {
 	return (
 		<div className="header">
 			<Navbar />
+
+			<div className="heading-wrapper">
+				<h1>
+					Creating an urban lifestyle <br /> built to last...
+				</h1>
+				<img src={underline} alt="" />
+				<p>The best place to purchase properties and buidling materials</p>
+				<Button variant="outlined">Get Started</Button>
+			</div>
 		</div>
 	);
 };
