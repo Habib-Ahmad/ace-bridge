bccbd83fdd71966e1a3b90ff645b90612ae0e143
Ahmad Habib <ahmadalkali69@gmail.com>
2022-08-05 22:03:37 +0100

    Properties component complete

--- src/components/Properties/index.jsx
+++ src/components/Properties/index.jsx
@@ -1,12 +1,100 @@
-import React from 'react';
+import { useRef } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import SectionHeader from '../SectionHeader';
+import PropertyCard from '../PropertyCard';
+import nextArrow from '../../assets/next-arrow.svg';
+import prevArrow from '../../assets/prev-arrow.svg';
 
 const Properties = () => {
+	const slider = useRef();
+
+	const settings = {
+		dots: false,
+		infinite: true,
+		speed: 1000,
+		arrows: true,
+		lazyLoading: true,
+		nextArrow: <NextArrow slider={slider} />,
+		prevArrow: <PrevArrow slider={slider} />,
+		className: 'custom-styles',
+		responsive: [
+			{
+				breakpoint: 1900,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 4,
+				},
+			},
+			{
+				breakpoint: 1500,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				},
+			},
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				breakpoint: 900,
+				settings: {
+					swipeToSlide: true,
+					arrows: false,
+					slidesToShow: 2,
+				},
+			},
+			{
+				breakpoint: 750,
+				settings: {
+					swipeToSlide: true,
+					arrows: false,
+					slidesToShow: 1,
+				},
+			},
+		],
+	};
+
 	return (
 		<div id="properties" className="properties">
 			<SectionHeader title="Properties" />
+
+			<Slider {...settings} ref={slider}>
+				{CardData.map((card) => (
+					<PropertyCard key={card.id} {...card} />
+				))}
+			</Slider>
 		</div>
 	);
 };
 
 export default Properties;
+
+const NextArrow = ({ slider }) => (
+	<div style={{ cursor: 'pointer' }} onClick={() => slider.current.slickNext()}>
+		<img src={nextArrow} alt="next" />
+	</div>
+);
+const PrevArrow = ({ slider }) => (
+	<div style={{ cursor: 'pointer' }} onClick={() => slider.current.slickPrev()}>
+		<img src={prevArrow} alt="previous" />
+	</div>
+);
+
+const CardData = new Array(40).fill('').map((card, index) => ({
+	id: index,
+	tag: 'For sale',
+	img: 'https://houseplanng.com/wp-content/uploads/wp-realestate-uploads/_property_gallery/2021/02/4-bedroom-bungalow-hp1-1-1024x652.jpg',
+	type: 'Office space',
+	title: 'Alison building apartment',
+	price: '60,800,000',
+	location: 'Asokoro, Abuja',
+	bathrooms: '3',
+	bedrooms: '3',
+	parking: '3',
+}));
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,23 +6,10 @@ const Home = () => {
 		<>
 			<Header />
 
-			<div style={{ padding: '0 10vw' }}>
+			<div className="content-wrapper">
 				<Properties />
 			</div>
 
-			{/* <div style={{ margin: '40px' }}>
-				<PropertyCard
-					tag="For sale"
-					img="https://houseplanng.com/wp-content/uploads/wp-realestate-uploads/_property_gallery/2021/02/4-bedroom-bungalow-hp1-1-1024x652.jpg"
-					type="Office space"
-					title="Alison building apartment"
-					price="60,800,000"
-					location="Asokoro, Abuja"
-					bathrooms="3"
-					bedrooms="3"
-					parking="3"
-				/>
-			</div> */}
 			<div style={{ height: '200px' }}></div>
 		</>
 	);
