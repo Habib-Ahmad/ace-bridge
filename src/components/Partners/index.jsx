b82574f46546bbbd14213920d71ef683241fdae8
Ahmad Habib <ahmadalkali69@gmail.com>
2022-08-09 21:14:03 +0100

    Changed partner logos

--- src/components/Partners/index.jsx
+++ src/components/Partners/index.jsx
@@ -1,23 +1,23 @@
-import logo1 from '../../assets/partners/logo1.png';
-import logo2 from '../../assets/partners/logo2.png';
-import logo3 from '../../assets/partners/logo3.png';
-import logo5 from '../../assets/partners/logo5.png';
+import mira from '../../assets/partners/mira.png';
+import richfield from '../../assets/partners/richfield.png';
+import surgical from '../../assets/partners/surgical.png';
+import switchbox from '../../assets/partners/switchbox.png';
 import algorizmi from '../../assets/partners/algorizmi.png';
 
 const Partners = () => {
 	return (
 		<div className="partners">
 			<div className="logo-wrapper">
-				<img src={logo1} alt="" />
-				<img src={logo2} alt="" />
-				<img src={logo3} alt="" />
-				<img src={logo5} alt="" />
-				<img src={algorizmi} alt="" />
-				<img src={logo1} alt="" />
-				<img src={logo2} alt="" />
-				<img src={logo3} alt="" />
-				<img src={logo5} alt="" />
-				<img src={algorizmi} alt="" />
+				<img src={mira} alt="mira" />
+				<img src={richfield} alt="richfield" />
+				<img src={surgical} alt="surgical" />
+				<img src={switchbox} alt="switchbox" />
+				<img src={algorizmi} alt="algorizmi" />
+				<img src={mira} alt="mira" />
+				<img src={richfield} alt="richfield" />
+				<img src={surgical} alt="surgical" />
+				<img src={switchbox} alt="switchbox" />
+				<img src={algorizmi} alt="algorizmi" />
 			</div>
 		</div>
 	);
