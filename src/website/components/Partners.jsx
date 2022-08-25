b2a4b17e60cb415fd6e6d5a08f067ded0afc64ee
Ahmad Habib <ahmadalkali69@gmail.com>
2022-08-26 00:23:25 +0100

    Adjusted partners section

--- src/website/components/Partners.jsx
+++ src/website/components/Partners.jsx
@@ -1,6 +1,6 @@
-import mira from '../assets/partners/mira.png';
-import richfield from '../assets/partners/richfield.png';
-import surgical from '../assets/partners/surgical.png';
+import polaris from '../assets/partners/polaris.svg';
+import braverock from '../assets/partners/braverock.png';
+import technique from '../assets/partners/technique-urban.png';
 import switchbox from '../assets/partners/switchbox.png';
 import algorizmi from '../assets/partners/algorizmi.png';
 
@@ -8,16 +8,16 @@ const Partners = () => {
 	return (
 		<div className="partners">
 			<div className="logo-wrapper">
-				<img src={mira} alt="mira" />
-				<img src={richfield} alt="richfield" />
-				<img src={surgical} alt="surgical" />
-				<img src={switchbox} alt="switchbox" />
+				<img src={polaris} alt="polaris" />
+				<img src={braverock} alt="braverock" />
 				<img src={algorizmi} alt="algorizmi" />
-				<img src={mira} alt="mira" />
-				<img src={richfield} alt="richfield" />
-				<img src={surgical} alt="surgical" />
+				<img src={technique} alt="technique urban" />
 				<img src={switchbox} alt="switchbox" />
+				<img src={polaris} alt="polaris" />
+				<img src={braverock} alt="braverock" />
 				<img src={algorizmi} alt="algorizmi" />
+				<img src={technique} alt="technique urban" />
+				<img src={switchbox} alt="switchbox" />
 			</div>
 		</div>
 	);
